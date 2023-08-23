eb8585bde9799cbe505c07d1b818ffd1ede659b9
matthew <larochelle.matthew@gmail.com>
2023-08-23 16:11:39 -0700

    backend maps schema/routes

---
+++ backend/controllers/mapController.js
@@ -0,0 +1,100 @@
+const Map = require('../models/mapModel')
+const mongoose = require('mongoose')
+
+// get all maps
+const getMaps = async (req, res) => {
+    const user_id = req.user._id
+
+    const maps = await Map.find({ user_id }).sort({createdAt: -1})
+
+    res.status(200).json(maps)
+}
+
+
+// get a single map
+const getMap = async (req, res) => {
+    const { id } = req.params
+    
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({error: 'No such map'})
+    }
+
+    const map = await Map.findById(id)
+
+    if (!map) {
+        return res.status(404).json({error: 'No such map'})
+    }
+
+    res.status(200).json(map)
+}
+
+
+// create new map
+const createMap = async (req, res) => {
+    const {title} = req.body
+
+    let emptyFields = []
+
+    if (!title) {
+        emptyFields.push('title')
+    }
+    if(emptyFields.length > 0) {
+        return res.status(400).json({ error: 'Please fill in all the fields', emptyFields})
+    }
+
+    // add doc to db
+    try {
+        const user_id = req.user._id
+
+        const map = await Map.create({title, user_id})
+        res.status(200).json(map)
+    } catch (error) {
+        res.status(400).json({error: error.message})
+        console.log(error.message)
+    }
+}
+
+// delete a map
+const deleteMap = async (req, res) => {
+    const { id } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({error: 'No such map'})
+    }
+
+    const map = await Map.findOneAndDelete({_id: id})
+
+    if (!map) {
+        return res.status(400).json({error: 'No such map'})
+    }
+
+    res.status(200).json(map)
+}
+
+// update a map
+const updateMap = async (req, res) => {
+    const { id } = req.params
+
+    if (!mongoose.Types.ObjectId.isValid(id)) {
+        return res.status(404).json({error: 'No such map'})
+    }
+
+    const map = await Map.findOneAndUpdate({_id: id}, {
+        ...req.body
+    })
+
+    if (!map) {
+        return res.status(400).json({error: 'No such map'})
+    }
+
+    res.status(200).json(map)
+}
+
+
+module.exports = {
+    getMaps,
+    getMap,
+    createMap,
+    deleteMap,
+    updateMap
+}
--- backend/server.js
+++ backend/server.js
@@ -2,8 +2,10 @@ require('dotenv').config()
 
 const express = require('express')
 const mongoose = require('mongoose')
-const campaignRoutes = require('./routes/campaigns')
 const userRoutes = require('./routes/user')
+const campaignRoutes = require('./routes/campaigns')
+const mapRoutes = require('./routes/maps')
+
 
 // express app
 const app = express()
@@ -17,8 +19,10 @@ app.use((req, res, next) => {
 })
 
 // routes
-app.use('/api/campaigns/', campaignRoutes)
 app.use('/api/user', userRoutes)
+app.use('/api/campaigns/', campaignRoutes)
+app.use('/api/maps', mapRoutes)
+
 
 // connect to db
 mongoose.connect(process.env.MONGO_URI)
