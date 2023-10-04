0c278f75e3ac176d33f68904a2584609bd09092d
matthew <larochelle.matthew@gmail.com>
2023-10-04 09:17:03 -0700

    get dm list and player list working

--- frontend/src/components/start/campaignDetails/CampaignDetails.js
+++ frontend/src/components/start/campaignDetails/CampaignDetails.js
@@ -10,26 +10,49 @@ const CampaignDetails = ({ campaign }) => {
     // const { dispatch } = useCampaignsContext()
     const { user } = useAuthContext()
     const [dmUsername, setDmUsername] = useState('')
+    const [playerList, setPlayerList] = useState([])
     const [showID, setShowID] = useState('')
 
+
+    // This retrieves the DM's username, rather than a mongoID ... can we also get the player names in one function?
     useEffect(() => {
         const getDMusername = async () => {
             const id = campaign.dm;
-            // console.log(id);
             const response = await fetch(`/api/user/${id}`, {
                 headers: {
                     'Authorization': `Bearer ${user.token}`
                 }
             })
             const json = await response.json()
-            console.log(json.username)
             setDmUsername(json.username)
-        // const dmUsername = dm.username
-        // console.log("dm Username:", dmUsername)
         }
     getDMusername()
     }, [])
 
+
+    // This retrieves the Player's usernames, rather than a mongoIDs
+    useEffect(() => {
+        const getPlayerUsernames = async () => {
+
+            // For each player in the players list: campaign.players
+
+            // fetch that user's username
+            // push it to a variable/list on this page
+
+            // const id = campaign.dm;
+            const response = await fetch(`/api/user/${campaign.players}`, {
+                headers: {
+                    'Authorization': `Bearer ${user.token}`
+                }
+            })
+            const json = await response.json()
+            setPlayerList(playerList + json)
+        }
+    getPlayerUsernames()
+    }, [])
+
+
+    // This deletes the DM's campaign
     const handleClick = async () => {
         if (!user) {
             return
@@ -47,11 +70,14 @@ const CampaignDetails = ({ campaign }) => {
         }
     }
 
+    // This shows the campaign ID so the user can send it to other players
     const giveID = () => {
         setShowID(prevValue => !prevValue)
     }
     
-    const path = `/campaign/${campaign._id}`
+    // We can use this link later to go to the actual campaign
+    // const path = `/campaign/${campaign._id}`
+
     return (
         <div className="campaignDetails__container">
             <h4>"Campaign Title" {campaign.title}</h4>
