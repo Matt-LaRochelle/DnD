f9e1e1c4444cd523bcdf2f942fc50b7d15b839ae
matthew <larochelle.matthew@gmail.com>
2023-10-04 14:38:38 -0700

    refactor password reset backend

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -14,10 +14,8 @@ const loginUser = async (req, res) => {
 
     try {
         const user = await User.login(email, password)
-
         // create a token
         const token = createToken(user._id)
-
         res.status(200).json({email, username: user.username, token, id: user._id})
     } catch (error) {
         res.status(400).json({error: error.message})
@@ -30,10 +28,8 @@ const signupUser = async (req, res) => {
 
     try {
         const user = await User.signup(email, username, password)
-
         // create a token
         const token = createToken(user._id)
-
         res.status(200).json({email, username, token, id: user._id})
     } catch (error) {
         res.status(400).json({error: error.message})
@@ -44,20 +40,16 @@ const signupUser = async (req, res) => {
 // send reset password link to email
 const forgotUser = async (req, res) => {
     const email = req.body.email
-    console.log("step 1: email: " + email)
 
     try {
         const user = await User.forgot(email)
-        console.log("step 2: user: " + user)
-        console.log("step 3: user.id: " + user._id)
 
         // create a token
         const token = jwt.sign({ userID: user._id }, process.env.SECRET, { expiresIn: '1h' })
-        console.log("step 4: token: " + token)
         user.resetPasswordToken = token;
         user.resetPasswordExpires = Date.now() + 3600000; // Expires in 1 hour
         await user.save();
-        console.log("step 5: saved user data")
+
         // create the email
         const message = {
             to: user.email,
@@ -65,15 +57,16 @@ const forgotUser = async (req, res) => {
             subject: 'Password reset link',
             html: `<p>Click <a href="https://localhost:3000/reset-password/${user.resetPasswordToken}">here</a> to reset your password.</p>`,
           };
+
         // send the email
         sgMail.send(message)
         .then(() => {
-            console.log('step 6: Email sent');
+            console.log('Email sent');
         })
         .catch((error) => {
-            console.error('step 6 failed: ' + error);
+            console.error('Email failed: ' + error);
         });
-        res.status(200).json({message: "step 7: backend got it!"})
+        res.status(200).json({message: "Email sent!"})
 
     } catch (error) {
         res.status(400).json({error: error.message})
@@ -83,41 +76,43 @@ const forgotUser = async (req, res) => {
 // verifies link from email
 const verifyLink = async (req, res) => {
     const token = req.body.token;
-    console.log(token)
+
     try {
-            // Look up the user by token
-    const user = await User.findOne({ resetPasswordToken: token, resetPasswordExpires: { $gt: Date.now() } });
-    if (!user) {
-      // If the token is invalid or has expired, render an error message or redirect to an error page
-      return res.status(400).send('Invalid or expired password reset token');
+        // Look up the user by token
+        const user = await User.findOne({ resetPasswordToken: token, resetPasswordExpires: { $gt: Date.now() } });
+       
+        if (!user) {
+            // If the token is invalid or has expired, render an error message or redirect to an error page
+            return res.status(400).send('Invalid or expired password reset token');
+        }
+
+        // Render the password reset form with the token as a hidden input
+        res.json({ token });
+    } catch (err) {
+        // Handle any errors that occur
+        console.error(err);
+        res.status(400).json({error: err.message})
     }
-    // Render the password reset form with the token as a hidden input
-    res.json({ token });
-  } catch (err) {
-    // Handle any errors that occur
-    console.error(err);
-    res.status(400).json({error: err.message})
-  }
 }
 
 // reset the password
 const resetPassword = async (req, res) => {
     const { token, password }= req.body
-    console.log("token: " + token, "password: " + password)
+
     try {
         // Look up the user by token
-    const validUser = await User.findOne({ resetPasswordToken: token, resetPasswordExpires: { $gt: Date.now() } });
-    if (!validUser) {
-    // If the token is invalid or has expired, render an error message or redirect to an error page
-    return res.status(400).send('Invalid or expired password reset token');
-    }
-    // Render the password reset form with the token as a hidden input
-    const user = await User.reset(token, password)
-    res.json({ user });
+        const validUser = await User.findOne({ resetPasswordToken: token, resetPasswordExpires: { $gt: Date.now() } });
+        if (!validUser) {
+            // If the token is invalid or has expired, render an error message or redirect to an error page
+            return res.status(400).send('Invalid or expired password reset token');
+        }
+        // Render the password reset form with the token as a hidden input
+        const user = await User.reset(token, password)
+        res.json({ user });
     } catch (err) {
-    // Handle any errors that occur
-    console.error(err);
-    res.status(400).json({error: err.message})
+        // Handle any errors that occur
+        console.error(err);
+        res.status(400).json({error: err.message})
     }
 }
 
@@ -126,6 +121,7 @@ const checkCookies = async (req, res) => {
     res.status(200).json({message: "You're logged in!"})
 } 
 
+// Get single User object
 const getUser = async (req, res) => {
     // Get the users ID
     const { id } = req.params
