85098b40086b3af0a8de517879ebf7204efefe03
matthew <larochelle.matthew@gmail.com>
2024-01-30 14:25:58 -0800

    add no avatar icon

--- frontend/src/components/npc/Npcs.js
+++ frontend/src/components/npc/Npcs.js
@@ -58,6 +58,7 @@ const Npcs = () => {
             {campaigns.dmID === user.id && 
                 <div className="npc" >
                     <h3>Add NPC</h3>
+                    <img src="https://media.istockphoto.com/id/1451587807/vector/user-profile-icon-vector-avatar-or-person-icon-profile-picture-portrait-symbol-vector.jpg?s=612x612&w=0&k=20&c=yDJ4ITX1cHMh25Lt1vI1zBn2cAKKAlByHBvPJ8gEiIg=" alt="Add NPC" />
                     <p onClick={handleClick} className='add'>+</p>
                 </div>}
         </div>
