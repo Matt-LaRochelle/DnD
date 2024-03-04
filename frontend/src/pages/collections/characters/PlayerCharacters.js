791c96d0fa48fe8b2c267014c5e6e05c7e0c6bab
matthew <larochelle.matthew@gmail.com>
2024-03-04 11:30:00 -0800

    refactor player characters

--- frontend/src/pages/collections/characters/PlayerCharacters.js
+++ frontend/src/pages/collections/characters/PlayerCharacters.js
@@ -1,42 +1,23 @@
 import './characters.css'
-
-import { useEffect, useState } from "react"
+import { useState } from "react"
 import { useNavigate } from "react-router-dom"
+
+// Hooks
 import { useCampaignsContext } from '../../../hooks/useCampaignsContext'
 import { usePcsContext } from '../../../hooks/usePcsContext'
-import { useNpcsContext } from '../../../hooks/useNpcsContext'
 import { useAuthContext } from '../../../hooks/useAuthContext'
+
+// Components
+import Loading from '../../../components/loading/Loading'
 
 const PlayerCharacters = () => {
     const { user } = useAuthContext()
-    const {campaigns, dispatch} = useCampaignsContext()
-    const { pcs, dispatch: pcsDispatch } = usePcsContext()
-    const { npcs, dispatch: npcsDispatch } = useNpcsContext()
+    const {campaigns } = useCampaignsContext()
+    const { pcs, dispatch } = usePcsContext()
+
     const navigate = useNavigate()
 
-    const [loading, setLoading] = useState(true)
-
-    useEffect(() => {
-        const fetchPcs = async () => {
-            setLoading(true);
-            const response = await fetch(`https://dnd-kukm.onrender.com/api/pc/${campaigns._id}`, {
-                headers: {
-                    'Authorization': `Bearer ${user.token}`
-                }
-            })
-            const json = await response.json()
-
-            if (response.ok) {
-                pcsDispatch({type: 'SET_PCS', payload: json})
-                setLoading(false)
-            }
-        }
-
-        if (user) {
-            fetchPcs()
-        }
-    }, [campaigns, user, dispatch])
-
+    const [loading, setLoading] = useState(false)
 
     const deletePC = async (id) => {
         if (!user) {
@@ -52,13 +33,11 @@ const PlayerCharacters = () => {
         const json = await response.json()
 
         if (response.ok) {
-            pcsDispatch({type: 'DELETE_PC', payload: json})
+            dispatch({type: 'DELETE_PC', payload: json})
             setLoading(false)
             
         }
     }
-
-
 
     const morePcInfo = (id) => {
         navigate(`/pc/${id}`);
@@ -68,83 +47,35 @@ const PlayerCharacters = () => {
         navigate(`/pc/add`);
     }
 
-
-
-    useEffect(() => {
-        const fetchNpcs = async () => {
-            setLoading(true);
-            const response = await fetch(`https://dnd-kukm.onrender.com/api/npc/${campaigns._id}`, {
-                headers: {
-                    'Authorization': `Bearer ${user.token}`
-                }
-            })
-            const json = await response.json()
-
-            if (response.ok) {
-                npcsDispatch({type: 'SET_NPCS', payload: json})
-                setLoading(false)
-            }
-        }
-
-        if (user) {
-            fetchNpcs()
-        }
-    }, [campaigns, user, dispatch])
-
-
-    const deleteNPC = async (id) => {
-        if (!user) {
-            return
-        }
-        setLoading(true);
-        const response = await fetch(`https://dnd-kukm.onrender.com/api/npc/${id}`, {
-            method: 'DELETE', // Specify the method here
-            headers: {
-                'Authorization': `Bearer ${user.token}`
-            }
-        })
-        const json = await response.json()
-
-        if (response.ok) {
-            npcsDispatch({type: 'DELETE_NPC', payload: json})
-            setLoading(false)
-            
-        }
-    }
-
-    const moreNpcInfo = (id) => {
-        navigate(`/npc/${id}`);
-    }
-
-    const handleNpcClick = () => {
-        navigate(`/npc/add`);
-    }
-
-
     return (
         <div className="characters__container glass">
-            <h2>Player Characters</h2>
-            <div className="characters__flexy">
-                {!loading && pcs.filter((pc) => {
-                    if (user.id === campaigns.dmID || pc.userID === user.id) {
-                        return true; // Include all PCs
-                    } else {
-                        return !pc.hidden; // Exclude PCs with hidden=true
-                    }
-                }).map((pc) => (
-                    <div className={pc.hidden ? "npc npc-hidden" : "npc"} key={pc._id}>
-                        <h3>{pc.name}</h3>
-                        <img src={pc.image} alt={pc.name} />
-                        <button className='button-primary' onClick={() => morePcInfo(pc._id)}>More Info</button>
-                        {campaigns.dmID === user.id && <button className="button-secondary" onClick={() => deletePC(pc._id)}>Delete</button> || pc.userID === user.id && <button className="button-secondary" onClick={() => deletePC(pc._id)}>Delete</button>}
+            {loading
+            ?   <Loading />
+            :   <div>
+                    <h2>Player Characters</h2>
+                    <div className="characters__flexy">
+                        {pcs.filter((pc) => {
+                            if (user.id === campaigns.dmID || pc.userID === user.id) {
+                                return true; // Include all PCs
+                            } else {
+                                return !pc.hidden; // Exclude PCs with hidden=true
+                            }
+                        }).map((pc) => (
+                            <div className={pc.hidden ? "npc npc-hidden" : "npc"} key={pc._id}>
+                                <h3>{pc.name}</h3>
+                                <img src={pc.image} alt={pc.name} />
+                                <button className='button-primary' onClick={() => morePcInfo(pc._id)}>More Info</button>
+                                {campaigns.dmID === user.id && <button className="button-secondary" onClick={() => deletePC(pc._id)}>Delete</button> || pc.userID === user.id && <button className="button-secondary" onClick={() => deletePC(pc._id)}>Delete</button>}
+                            </div>
+                            ))}
+                        <div className="npc" >
+                            <h3>Add PC</h3>
+                            <img src="https://media.istockphoto.com/id/1451587807/vector/user-profile-icon-vector-avatar-or-person-icon-profile-picture-portrait-symbol-vector.jpg?s=612x612&w=0&k=20&c=yDJ4ITX1cHMh25Lt1vI1zBn2cAKKAlByHBvPJ8gEiIg=" alt="Add PC" />
+                            <p onClick={handlePcClick} className='add'>+</p>
+                        </div>
                     </div>
-                    ))}
-                <div className="npc" >
-                    <h3>Add PC</h3>
-                    <img src="https://media.istockphoto.com/id/1451587807/vector/user-profile-icon-vector-avatar-or-person-icon-profile-picture-portrait-symbol-vector.jpg?s=612x612&w=0&k=20&c=yDJ4ITX1cHMh25Lt1vI1zBn2cAKKAlByHBvPJ8gEiIg=" alt="Add PC" />
-                    <p onClick={handlePcClick} className='add'>+</p>
                 </div>
-            </div>
+            }
         </div>
     )
 }
