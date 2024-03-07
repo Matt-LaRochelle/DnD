01fc36b1983f077b0f48306b4d1513e110c07f05
matthew <larochelle.matthew@gmail.com>
2024-03-07 11:53:17 -0800

    create faces for a D6

---
+++ frontend/src/components/dice/D6.js
@@ -0,0 +1,68 @@
+import './dice.css';
+
+const D6 = () => {
+    return (
+        <div className="dice6">
+            <div className="dice-face">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div className="dice-dot"></div>
+            </div>
+            <div className="dice-face">
+                <div></div>
+                <div></div>
+                <div className="dice-dot"></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div className="dice-dot"></div>
+            </div>
+            <div className="dice-face">
+                <div></div>
+                <div></div>
+                <div className="dice-dot"></div>
+                <div></div>
+                <div className="dice-dot"></div>
+                <div></div>
+                <div className="dice-dot"></div>
+            </div>
+            <div className="dice-face">
+                <div className="dice-dot"></div>
+                <div></div>
+                <div className="dice-dot"></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div className="dice-dot"></div>
+                <div></div>
+                <div className="dice-dot"></div>
+            </div>
+            <div className="dice-face">
+                <div className="dice-dot"></div>
+                <div></div>
+                <div className="dice-dot"></div>
+                <div></div>
+                <div className="dice-dot"></div>
+                <div></div>
+                <div className="dice-dot"></div>
+                <div></div>
+                <div className="dice-dot"></div>
+            </div>
+            <div className="dice-face">
+                <div className="dice-dot"></div>
+                <div></div>
+                <div className="dice-dot"></div>
+                <div className="dice-dot"></div>
+                <div></div>
+                <div className="dice-dot"></div>
+                <div className="dice-dot"></div>
+                <div></div>
+                <div className="dice-dot"></div>
+            </div>
+        </div>
+    )
+}
+
+export default D6;
--- frontend/src/pages/dice/Dice.js
+++ frontend/src/pages/dice/Dice.js
@@ -1,16 +1,20 @@
 import './dice.css'
+import D6 from '../../components/dice/D6'
 
 const Dice = () => {
     return (
-        <div className="dice__container">
-            <h1>Dice</h1>
-            <p>D4</p>
-            <p>D6</p>
-            <p>D8</p>
-            <p>D10</p>
-            <p>D12</p>
-            <p>D20</p>
-            <p>D100</p>
+        <div className="dice__page">
+            <div className="dice__container">
+                <h1>Dice</h1>
+                <p>D4</p>
+                <p>D6</p>
+                <p>D8</p>
+                <p>D10</p>
+                <p>D12</p>
+                <p>D20</p>
+                <p>D100</p>
+            </div>
+            <D6 />
         </div>
     )
 }
